d5e2c09b2009ee77aaf710a50a99d7b87f1453ec
Paulo H Madeira <paulin.mad@hotmail.com>
2021-06-24 16:51:58 -0300

    Requests created: get tour by id; update tour and delete tour. Code refactored.Custom  Middleware implementated.

--- app.js
+++ app.js
@@ -3,25 +3,58 @@ const express = require('express');
 
 const app = express();
 
-//use a middleware to use json
+//use a middleware to use json body
 app.use(express.json());
+
+//create my own middleware
+app.use((req, res, next) => {
+  console.log('Middleware message');
+  next();
+})
+
+//add Request time using middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+})
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-//Listen to GET requests on tours endpoint
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   //send the response with a json using JSend format
   res.status(200).json({
     status: 'success',
+    requestedAt:req.requestTime,
     results: tours.length,
     data: { tours },
   });
-});
+}
 
-//Listen to POST requests on tours endpoint
-app.post('/api/v1/tours', (req, res) => {
+const getTour = (req, res) => {
+  const id = +req.params.id
+
+  // find the tour by the id sent in the parameter
+  const tour = tours.find(item => item.id === id)
+  
+  //check if the id is greater than the array and return 'not found' status
+  // if (id > tours.length) 
+  //if tour is null ou empty, return 'not found' status
+    if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      requestedAt:req.requestTime,
+      message:'Invalid ID'
+    })
+  }
+  //send the response with a json using JSend format
+  res.status(200).json({
+    status: 'success',  requestedAt:req.requestTime,  
+    data: { tour },
+  });
+}
+const createTour =(req, res) => {
   //get the id of the last tour and add 1 to get the new tour id
   const newId = tours[tours.length - 1].id + 1;
   // create a variable using the object from the req.body and the id of the new tour
@@ -34,10 +67,57 @@ app.post('/api/v1/tours', (req, res) => {
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     (err) => {
-      res.status(201).json({ status: 'success', data: { tour: newTour } });
+      res.status(201).json({ status: 'success',requestedAt:req.requestTime, data: { tour: newTour } });
     }
   );
-});
+}
+
+const updateTour= (req, res) => {
+
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',requestedAt:req.requestTime,
+      message: 'Invalid ID'
+    })
+  }
+
+  res.status(200).json({
+    status: 'success',requestedAt:req.requestTime,
+    data:{tour:'<Updated tour...>'}
+  })
+}
+
+const deleteTour =(req, res) => {
+
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',requestedAt:req.requestTime,
+      message: 'Invalid ID'
+    })
+  }
+//status 204 = 'no content'
+  res.status(204).json({
+    status: 'success',requestedAt:req.requestTime,
+    data:null
+  })
+}
+
+// //Listen to GET requests on tours endpoint
+// app.get('/api/v1/tours', getAllTours);
+// //GET request using parameter to receive only a tour by ID
+// app.get('/api/v1/tours/:id', getTour);
+// //Listen to POST requests on tours endpoint
+// app.post('/api/v1/tours', createTour);
+// //Patch request to update a tour by ID
+// app.patch('/api/v1/tours/:id', updateTour)
+// //Delete request to update a tour by ID
+// app.delete('/api/v1/tours/:id', deleteTour)
+
+
+
+//chaining requests on a single route
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour)
 
 const PORT = 3000;
 
